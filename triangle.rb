0f1969731fe503185433ba3fb5c0c37bb9f77091
Shaun Bennett <me@phinary.ca>
2019-05-03 20:49:10 -0400

    Add control statements, exceptions, triangle problem

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,26 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  num_equal = 0
+
+  if a <= 0 || b <= 0 || c <= 0
+    raise TriangleError, "Cannot have a side with 0 or less length"
+  end
+
+  sorted = [a, b, c].sort
+  if sorted[2] >= sorted[0] + sorted[1]
+    raise TriangleError, "Pythagorean Theorem does not hold, this is not a triangle"
+  end
+  
+  num_equal += 1 if a == b
+  num_equal += 1 if a == c
+  num_equal += 1 if b == c
+
+  case num_equal
+  when 1 then :isosceles
+  when 3 then :equilateral
+  else :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
